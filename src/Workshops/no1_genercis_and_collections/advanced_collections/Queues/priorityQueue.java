09181c641e820f80987efdd3c208240dec823293
0xRobert <flaksik123@gmail.com>
2022-12-07 13:15:51 +0100

    queues

---
+++ src/Workshops/no1_genercis_and_collections/advanced_collections/Queues/priorityQueue.java
@@ -0,0 +1,2 @@
+package Workshops.no1_genercis_and_collections.advanced_collections.Queues;public class priorityQueue {
+}
---
+++ src/Workshops/no1_genercis_and_collections/advanced_collections/Queues/queueAndDeque.java
@@ -0,0 +1,40 @@
+package Workshops.no1_genercis_and_collections.advanced_collections.Queues;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.List;
+import java.util.Queue;
+
+public class Main {
+
+    public static void main(String[] args) {
+        List<Integer> input = List.of(1, 5, 9, 1, 62);
+
+        Queue<Integer> queue = new ArrayDeque<>();
+        /*  peek() - zwraca pierwszy element kolejki
+            poll() - zwraca i usuwa pierwszy element albo null jeśli queue jest pusta
+            removes() - -,,- ale nie zwraca nulla
+         */
+
+        Deque<Integer> stack = new ArrayDeque<>();
+        /*  push() - dodaje e z porzodu
+            peek() - tak samo zwraca pierwszy element
+            pop() -  usuwa pierwszy elemen
+         */
+
+
+        for (Integer element : input) {
+            queue.offer(element);
+         //   System.out.println("queue.offer " + queue.offer(element) + ", added element: " + element);
+         //   System.out.println(queue);
+        }
+
+        int size = queue.size();
+        for (int i = 0; i < size; i++) {
+            System.out.println("queue.peek: " + queue.peek());
+            System.out.println(queue);
+            System.out.println("queue.poll: " + queue.poll());
+            System.out.println(queue);
+        }
+    }
+}
