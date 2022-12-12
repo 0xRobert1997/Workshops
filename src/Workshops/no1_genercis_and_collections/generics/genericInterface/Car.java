947131e7e19240d3a4d03b2ddef086d47157df0c
0xRobert <105931731+0xRobert1997@users.noreply.github.com>
2022-12-12 16:04:18 +0100

    Delete src/Workshops/no1_genercis_and_collections directory

--- src/Workshops/no1_genercis_and_collections/generics/genericInterface/Car.java
+++
@@ -1,4 +0,0 @@
-package Workshops.no1_genercis_and_collections.generics.genericInterface;
-
-public class Car {
-}
--- src/Workshops/no1_genercis_and_collections/generics/genericInterface/Packable.java
+++
@@ -1,7 +0,0 @@
-package Workshops.no1_genercis_and_collections.generics.genericInterface;
-
-public interface Packable<T> {
-
-    void pack(T element);
-    T empty();
-}
