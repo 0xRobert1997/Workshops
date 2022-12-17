431d7f4431f646b4e5d2593d16121d01ffdecaae
0xRobert <flaksik123@gmail.com>
2022-12-17 21:22:10 +0100

    streamss

--- src/Workshops/no2_functional_programming/Streams/podsumowanieV1.java
+++ src/Workshops/no2_functional_programming/Streams/podsumowanieV1.java
@@ -1,2 +1,29 @@
-package Workshops.no2_functional_programming.Streams;public class podsumowanieV1 {
+package Workshops.no2_functional_programming.Streams;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class podsumowanie {
+
+    public static void main(String[] args) {
+        List<Integer> integerList = new ArrayList<>();
+         integerList.add(1);
+         integerList.add(2);
+         integerList.add(3);
+         integerList.add(3);
+         integerList.add(9);
+
+         Set<Integer> mySet = integerList.stream()
+                 .collect(
+                         () -> new TreeSet<>(),
+                         (currentSet, nextElem) -> currentSet.add(nextElem),
+                         (firstSet, secondSet) -> firstSet.addAll(secondSet)
+                 );
+
+         Integer result = integerList.stream()
+                 .reduce(0, (currentState, nextValue) -> currentState + nextValue);
+         //                     ^ jak tu nie będzie 1 argumentu (identity) to spodziewa się że wynikiem będzie optional
+    }
 }
--- src/Workshops/no2_functional_programming/Streams/podsumowanieV2.java
+++
@@ -1,2 +0,0 @@
-package Workshops.no2_functional_programming.Streams;public class podsumowanieV2 {
-}
