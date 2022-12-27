14203e708a4083e0355f0ab4768cb8c9c1874016
0xRobert <flaksik123@gmail.com>
2022-12-27 18:38:54 +0100

    files

---
+++ src/Workshops/no3_localization_and_files/IO_in_out/files.java
@@ -0,0 +1,35 @@
+package Workshops.no3_localization_and_files.IO_in_out;
+
+import java.io.File;
+import java.io.IOException;
+
+public class main {
+
+    public static void main(String[] args) {
+
+        File file1 = new File("./src/Workshops/xd.txt");
+        System.out.println(file1.exists());
+        System.out.println("file1: " + file1);
+        System.out.println("AbsolutePath: " + file1.getAbsolutePath());
+        System.out.println("AbsoluteFile: " + file1.getAbsoluteFile());
+        System.out.println("UsuableSpace: " + file1.getUsableSpace());
+        System.out.println("Parent: " + file1.getParent());
+
+        // rename przyjmuje file
+        file1.renameTo(new File("kekw.txt"));
+       // file1.delete();
+
+
+        File myFile = new File("nosrc/example/code.txt");
+        System.out.println(myFile.mkdir());
+        // zrobi folder o nazwie code.txt
+        // mkdirs tworzy wszystkie foldery po drodze
+        System.out.println(myFile.mkdirs());
+        File myFile2 = new File("zajav2.txt");
+        try {
+            System.out.println(myFile2.createNewFile());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/Workshops/no3_localization_and_files/IO_in_out/main.java
@@ -0,0 +1,2 @@
+package Workshops.no3_localization_and_files.IO_in_out;public class main {
+}
