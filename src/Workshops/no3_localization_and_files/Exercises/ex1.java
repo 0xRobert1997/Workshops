bbad2fc143edf598a94136a3b4a639a143fef34a
0xRobert <flaksik123@gmail.com>
2023-01-05 20:48:46 +0100

    zad1

---
+++ src/Workshops/no3_localization_and_files/Exercises/ex1.java
@@ -0,0 +1,9 @@
+package Workshops.no3_localization_and_files.Exercises;
+
+public class main {
+
+    public static void main(String[] args) {
+
+
+    }
+}
--- src/Workshops/no3_localization_and_files/Exercises/main.java
+++
@@ -1,2 +0,0 @@
-package Workshops.no3_localization_and_files.Exercises;public class main {
-}
