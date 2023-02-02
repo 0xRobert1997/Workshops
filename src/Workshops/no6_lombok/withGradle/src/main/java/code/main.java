8ddb13f1d1c1f219354d72c8e878c93c2e435d6f
0xRobert <flaksik123@gmail.com>
2023-02-02 18:39:58 +0100

    one

--- src/Workshops/no6_lombok/withGradle/src/main/java/code/main.java
+++ src/Workshops/no6_lombok/withGradle/src/main/java/code/main.java
@@ -1,2 +1,12 @@
-package code;public class main {
+package code;
+
+public class main {
+    public static void main(String[] args) {
+
+       // Dog burek = new Dog("Burek", 2, new Owner());
+        Dog kurek = Dog.of("kurek", 12, new Owner());
+
+
+
+    }
 }
