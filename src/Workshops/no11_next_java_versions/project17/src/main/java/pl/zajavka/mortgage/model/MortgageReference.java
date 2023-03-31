2431031925bf88439937de3a3d1f81d656a34242
0xRobert <flaksik123@gmail.com>
2023-03-31 18:53:09 +0200

    againproject

---
+++ src/Workshops/no11_next_java_versions/project17/src/main/java/pl/zajavka/mortgage/model/MortgageReference.java
@@ -0,0 +1,10 @@
+package pl.zajavka.mortgage.model;
+
+import java.math.BigDecimal;
+
+public record MortgageReference(
+        BigDecimal referenceAmount,
+        BigDecimal referenceDuration
+) {
+
+}
---
+++ src/Workshops/no11_next_java_versions/project17/src/main/java/pl/zajavka/mortgage/model/MortgageResidual.java
@@ -0,0 +1,10 @@
+package pl.zajavka.mortgage.model;
+
+import java.math.BigDecimal;
+
+public record MortgageResidual(
+        BigDecimal residualAmount,
+        BigDecimal residualDuration
+) {
+
+}
