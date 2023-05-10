863fa4a8aad3113901ae599d164846aa65d7a42c
0xRobert <flaksik123@gmail.com>
2023-05-10 12:26:03 +0200

    spring beans project

---
+++ src/Workshops/no13_spirng_beans/spring_project/src/main/java/pl/zajavka/mortgage/Configuration/CalculatorConfiguration.java
@@ -0,0 +1,40 @@
+package pl.zajavka.mortgage.Configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import pl.zajavka.mortgage.MortgageCalculator;
+import pl.zajavka.mortgage.model.Rate;
+import pl.zajavka.mortgage.model.RateAmounts;
+import pl.zajavka.mortgage.model.Summary;
+import pl.zajavka.mortgage.services.SummaryService;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.function.Function;
+
+@Configuration
+@ComponentScan(basePackageClasses = MortgageCalculator.class)
+public class CalculatorConfiguration {
+
+    @Bean
+    public static SummaryService create() {
+        return rates -> {
+            BigDecimal interestSum = calculate(rates, rate -> rate.rateAmounts().interestAmount());
+            BigDecimal overpaymentProvisionSum = calculate(rates, rate -> rate.rateAmounts().overpayment().provisionAmount());
+            BigDecimal totalLostSum = interestSum.add(overpaymentProvisionSum);
+            BigDecimal totalCapital = calculate(rates, rate -> totalCapital(rate.rateAmounts()));
+            return new Summary(interestSum, overpaymentProvisionSum, totalLostSum, totalCapital);
+        };
+    }
+
+    private static BigDecimal totalCapital(final RateAmounts rateAmounts) {
+        return rateAmounts.capitalAmount().add(rateAmounts.overpayment().amount());
+    }
+
+    private static BigDecimal calculate(final List<Rate> rates, Function<Rate, BigDecimal> function) {
+        return rates.stream()
+                .reduce(BigDecimal.ZERO, (sum, next) -> sum.add(function.apply(next)), BigDecimal::add);
+    }
+
+}
