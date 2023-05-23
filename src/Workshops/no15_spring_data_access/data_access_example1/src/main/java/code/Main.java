cffe7e711ed765b6caae6722ed11edf29c5868d5
0xRobert <flaksik123@gmail.com>
2023-05-23 20:41:19 +0200

    data_access 2

--- src/Workshops/no15_spring_data_access/data_access_example1/src/main/java/code/Main.java
+++ src/Workshops/no15_spring_data_access/data_access_example1/src/main/java/code/Main.java
@@ -1,14 +1,25 @@
 package code;
 
 public class Main {
-/*
-    architektura trójwarstwowa - three-tier /
-    warstwa
-    -prezentacji
-    -logiki biz
-    -danych
-    DAO - data access object
- */
+    /*
+        architektura trójwarstwowa - three-tier /
+        warstwa
+        -prezentacji
+        -logiki biz
+        -danych
+        DAO - data access object
+
+        Transaction
+        ACID - atomicity(niepodzielność), Consistency(spójność), Isolation, Durability(trwałość)
+
+        efekty współbieżności tranzakcji - występują przy niskim poziomie izolacji transakcji
+
+        Dirty read
+            gdy jedna transakcja może odczytywać dane z wiersza, który został
+            zmodyfikowany przez inną działającą transakcję i nie został jeszcze zacommitowany
+        Non-repeatable read
+        Phantom read
+     */
     public static void main(String[] args) {
 
     }
