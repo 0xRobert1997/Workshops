17b94454f5435590f334684f9bac70273b09c417
0xRobert <flaksik123@gmail.com>
2023-07-12 20:05:19 +0200

    spring boot 2

---
+++ src/Workshops/w20_spring_boot/web-mvc-boot-example/src/main/java/pl/zajavka/controller/EmployeesController.java
@@ -0,0 +1,83 @@
+package pl.zajavka.controller;
+
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import pl.zajavka.infrastructure.database.entity.EmployeeEntity;
+import pl.zajavka.infrastructure.database.repository.EmployeeRepository;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/employees")
+@AllArgsConstructor
+public class EmployeesController {
+
+    private EmployeeRepository employeeRepository;
+
+    @GetMapping
+    public String employeesList(Model model) {
+        List<EmployeeEntity> allEmployees = employeeRepository.findAll();
+
+        model.addAttribute("employees", allEmployees);
+        model.addAttribute("employeeDTO", new EmployeeDTO());
+
+        return "employees";
+    }
+
+    @GetMapping("/show/{employeeId}")
+    public String showEmployeeDetails(@PathVariable Integer employeeId, Model model) {
+        EmployeeEntity employeeDetails = employeeRepository.findById(employeeId)
+                .orElseThrow(() -> new EntityNotFoundException(
+                        String.format("EmployeeEntity not found, employeeId: [%s]", employeeId)));
+
+        model.addAttribute("employee", employeeDetails);
+
+        return "employeeDetails";
+    }
+
+    @PostMapping("/add")
+    public String addEmployee(
+        @Valid @ModelAttribute("employeeDTO") EmployeeDTO employeeDTO
+    ) {
+        EmployeeEntity newEmployee = EmployeeEntity.builder()
+                .name(employeeDTO.getName())
+                .surname(employeeDTO.getSurname())
+                .salary(employeeDTO.getSalary())
+                .phone(employeeDTO.getPhone())
+                .email(employeeDTO.getEmail())
+                .build();
+        employeeRepository.save(newEmployee);
+
+        return "redirect:/employees";
+    }
+
+    @PutMapping("/update")
+    public String updateEmployee(
+        @Valid @ModelAttribute("employeeDTO") EmployeeDTO employeeDTO
+    ) {
+        EmployeeEntity existingEmployee = employeeRepository.findById(employeeDTO.getEmployeeId())
+                .orElseThrow(() -> new EntityNotFoundException(
+                        String.format("EmployeeEntity not found, employeeId: [%s]", employeeDTO.getEmployeeId())));
+
+        existingEmployee.setName(employeeDTO.getName());
+        existingEmployee.setSurname(employeeDTO.getSurname());
+        existingEmployee.setSalary(employeeDTO.getSalary());
+        existingEmployee.setPhone(employeeDTO.getPhone());
+        existingEmployee.setEmail(employeeDTO.getEmail());
+        employeeRepository.save(existingEmployee);
+
+        return "redirect:/employees";
+    }
+
+    @DeleteMapping("/delete/{employeeId}")
+    public String deleteEmployee(@PathVariable Integer employeeId) {
+        employeeRepository.deleteById(employeeId);
+
+        return "redirect:/employees";
+    }
+
+}
---
+++ src/Workshops/w20_spring_boot/web-mvc-boot-example/src/test/java/pl/zajavka/util/EntityFixtures.java
@@ -0,0 +1,37 @@
+package pl.zajavka.util;
+
+import lombok.experimental.UtilityClass;
+import pl.zajavka.infrastructure.database.entity.EmployeeEntity;
+
+import java.math.BigDecimal;
+
+@UtilityClass
+public class EntityFixtures {
+    public static EmployeeEntity someEmployee1() {
+        return EmployeeEntity.builder()
+                .name("Agnieszka")
+                .surname("Zajavkowa")
+                .salary(new BigDecimal("12345"))
+                .phone("+48 555 555 555")
+                .email("zajavka@zajavka.com")
+                .build();
+    }
+    public static EmployeeEntity someEmployee2() {
+        return EmployeeEntity.builder()
+                .name("Remek")
+                .surname("Spring")
+                .salary(new BigDecimal("321"))
+                .phone("+60 111 111 111")
+                .email("remek@xd.com")
+                .build();
+    }
+    public static EmployeeEntity someEmployee3() {
+        return EmployeeEntity.builder()
+                .name("Tadek")
+                .surname("Pepega")
+                .salary(new BigDecimal("100000000"))
+                .phone("+11 222 222 222")
+                .email("something@something.com")
+                .build();
+    }
+}
