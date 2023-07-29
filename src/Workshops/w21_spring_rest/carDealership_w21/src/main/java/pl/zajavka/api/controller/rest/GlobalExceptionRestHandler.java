6b79874221cffa746b2121ba969884151408eb5b
0xRobert <flaksik123@gmail.com>
2023-07-29 20:02:41 +0200

    w21 project config

---
+++ src/Workshops/w21_spring_rest/carDealership_w21/src/main/java/pl/zajavka/api/controller/rest/GlobalExceptionRestHandler.java
@@ -0,0 +1,66 @@
+package pl.zajavka.api.controller.rest;
+
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.validation.ConstraintViolation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.*;
+import org.springframework.lang.NonNull;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import pl.zajavka.api.dto.ExceptionMessage;
+
+import java.util.Map;
+import java.util.UUID;
+
+@Slf4j
+@RestControllerAdvice(annotations = RestController.class) // dopisek żeby obsługi błędów rest'a i normalnych controllerów mogły działać razem
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class GlobalExceptionRestHandler extends ResponseEntityExceptionHandler {
+
+    private static final Map<Class<?>, HttpStatus> EXCEPTION_STATUS = Map.of(
+            ConstraintViolation.class, HttpStatus.BAD_REQUEST,
+            EntityNotFoundException.class, HttpStatus.NOT_FOUND
+    );
+    @Override
+    protected ResponseEntity<Object> handleExceptionInternal(
+            @NonNull Exception ex,
+            Object body,
+            @NonNull HttpHeaders headers,
+            @NonNull HttpStatusCode statusCode,
+            @NonNull WebRequest request
+    ) {
+        final String errorId = UUID.randomUUID().toString();
+        log.error("Exception: ID={}, HttpStatus={}", errorId, statusCode, ex);
+        return super.handleExceptionInternal(
+                ex,
+                ExceptionMessage.of(errorId),
+                headers,
+                statusCode,
+                request
+        );
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<?> handle(Exception ex) {
+        return doHandle(ex, getHttpStatusFromException(ex.getClass()));
+    }
+
+    private ResponseEntity<?> doHandle(Exception ex, HttpStatus status) {
+        final String errorId = UUID.randomUUID().toString();
+        log.error("Exception: ID={}, HttpStatus={}", errorId, status, ex);
+        return ResponseEntity
+                .status(status)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(ExceptionMessage.of(errorId));
+    }
+
+    private HttpStatus getHttpStatusFromException(final Class<?> exceptionClass) {
+        return EXCEPTION_STATUS.getOrDefault(exceptionClass, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+}
---
+++ src/Workshops/w21_spring_rest/carDealership_w21/src/main/java/pl/zajavka/infrastructure/configuration/WebClientConfiguration.java
@@ -0,0 +1,70 @@
+package pl.zajavka.infrastructure.configuration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.codec.json.Jackson2JsonDecoder;
+import org.springframework.http.codec.json.Jackson2JsonEncoder;
+import org.springframework.web.reactive.function.client.ExchangeStrategies;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import pl.zajavka.infrastructure.cepik.ApiClient;
+import pl.zajavka.infrastructure.cepik.api.PojazdyApi;
+import pl.zajavka.infrastructure.cepik.api.SownikiApi;
+
+@Configuration
+public class WebClientConfiguration {
+
+    @Value("${api.cepik.url}")
+    private String cepikUrl;
+    public static final int TIMEOUT = 5000;
+
+    @Bean
+    public WebClient webClient(final ObjectMapper objectMapper) {
+/*        final var httpClient = HttpClient.create()
+                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, TIMEOUT)
+                .responseTimeout(Duration.ofMillis(TIMEOUT))
+                .doOnConnected(conn ->
+                        conn.addHandlerLast(new ReadTimeoutHandler(TIMEOUT, TimeUnit.MILLISECONDS))
+                                .addHandlerLast(new WriteTimeoutHandler(TIMEOUT, TimeUnit.MILLISECONDS)));*/
+
+        final var exchangeStrategies = ExchangeStrategies
+                .builder()
+                .codecs(configurer -> {
+                    configurer
+                            .defaultCodecs()
+                            .jackson2JsonEncoder(
+                                    new Jackson2JsonEncoder(objectMapper, MediaType.APPLICATION_JSON)
+                            );
+                    configurer
+                            .defaultCodecs()
+                            .jackson2JsonDecoder(
+                                    new Jackson2JsonDecoder(objectMapper, MediaType.APPLICATION_JSON)
+                            );
+                })
+                .build();
+        return WebClient.builder()
+                .exchangeStrategies(exchangeStrategies)
+                .build();
+    }
+
+    // ApiClient jest wygenerowany w tej paczce z cepikiem
+    @Bean
+    public ApiClient apiClient(final WebClient webClient) {
+        ApiClient apiClient = new ApiClient(webClient);
+        apiClient.setBasePath(cepikUrl);
+        return apiClient;
+
+    }
+
+    @Bean
+    public SownikiApi sownikiApi(final ApiClient apiClient) {
+        return new SownikiApi(apiClient);
+    }
+    @Bean
+    public PojazdyApi pojazdyApi(final ApiClient apiClient) {
+        return new PojazdyApi(apiClient);
+    }
+}
